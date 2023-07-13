6a87ffe809bc55ed523174ec7ca3d545f597f58d
Lucas Almeida <44484333+me-luc@users.noreply.github.com>
2023-07-13 15:00:29 +0000

    feat: update main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import SearchParam from "./SearchParam";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [beginDate, setBeginDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [operatorName, setOperatorName] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchParam
+        value={beginDate}
+        setValue={setBeginDate}
+        valueName="Data de início"
+      />
+      <SearchParam
+        value={endDate}
+        setValue={setEndDate}
+        valueName="Data de fim"
+      />
+      <SearchParam
+        value={operatorName}
+        setValue={setOperatorName}
+        valueName="Nome operador transacionado"
+      />
+      <button>Pesquisar</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
