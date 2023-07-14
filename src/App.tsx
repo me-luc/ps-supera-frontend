fcc075fce7427163c9f279e8460f8497ddd55234
me.luc <lukasalmeida372@gmail.com>
2023-07-13 22:57:29 -0300

    feat: update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,37 @@
-import { useState } from "react";
-import SearchParam from "./SearchParam";
+import { useState } from 'react';
+import SearchParam from './SearchParam';
+import DataTable from './DataTable';
 
 function App() {
-  const [beginDate, setBeginDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [operatorName, setOperatorName] = useState("");
+	const [beginDate, setBeginDate] = useState('');
+	const [endDate, setEndDate] = useState('');
+	const [operatorName, setOperatorName] = useState('');
 
-  return (
-    <>
-      <SearchParam
-        value={beginDate}
-        setValue={setBeginDate}
-        valueName="Data de início"
-      />
-      <SearchParam
-        value={endDate}
-        setValue={setEndDate}
-        valueName="Data de fim"
-      />
-      <SearchParam
-        value={operatorName}
-        setValue={setOperatorName}
-        valueName="Nome operador transacionado"
-      />
-      <button>Pesquisar</button>
-    </>
-  );
+	return (
+		<>
+			<div className='search-items-box'>
+				<SearchParam
+					setValue={setBeginDate}
+					valueName='Data de início'
+					inputAttributes={{ type: 'date' }}
+				/>
+				<SearchParam
+					setValue={setEndDate}
+					valueName='Data de fim'
+					inputAttributes={{ type: 'date' }}
+				/>
+				<SearchParam
+					setValue={setOperatorName}
+					valueName='Nome operador transacionado'
+				/>
+			</div>
+			<div className='search-button-box'>
+				<button className='search-button'>Pesquisar</button>
+			</div>
+
+			<DataTable />
+		</>
+	);
 }
 
 export default App;
