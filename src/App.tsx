32aaf0e54a8c363c56fa503bec1295a7b7050c1f
me.luc <lukasalmeida372@gmail.com>
2023-07-18 18:15:26 -0300

    feat: add api data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,43 @@ import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import SearchParam from './SearchParam';
 import DataTable from './DataTable';
-import { Operation, OperationData } from './types';
+import { Operation } from './types';
 import { calculateBalance, getType } from './utils';
+import { fetchData } from './service';
 
 function App() {
 	const [beginDate, setBeginDate] = useState('');
 	const [endDate, setEndDate] = useState('');
 	const [operatorName, setOperatorName] = useState('');
 	const [data, setData] = useState<Operation[]>([]);
-	const [filteredData, setFilteredData] = useState<Operation[]>([]);
 	const [totalBalance, setTotalBalance] = useState(0);
+	const [initialFetch, setInitialFetch] = useState(false);
 
-	useEffect(() => {
-		const data = getData().map((item) => {
+	async function fetch() {
+		const response = await fetchData({
+			operatorName,
+			beginDate,
+			endDate,
+		});
+		const formatted = response.map((item) => {
 			return {
 				...item,
 				type: getType(item.type),
 			};
 		});
-		setData(data);
-		setFilteredData(data);
+		setData(formatted);
+	}
 
-		const total = calculateBalance(data);
-		setTotalBalance(total);
+	useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-floating-promises
+		fetch().then(() => setInitialFetch(true));
 	}, []);
+
+	useEffect(() => {
+		if (!initialFetch) return;
+		const balance = calculateBalance(data);
+		setTotalBalance(balance);
+	}, [initialFetch]);
 
 	return (
 		<>
@@ -46,170 +59,18 @@ function App() {
 				/>
 			</div>
 			<div className='search-button-box'>
-				<button className='search-button' onClick={changeSearch}>
+				<button className='search-button' onClick={search}>
 					Pesquisar
 				</button>
 			</div>
 
-			<DataTable data={filteredData} totalBalance={totalBalance} />
+			<DataTable data={data} totalBalance={totalBalance} />
 		</>
 	);
 
-	function changeSearch() {
-		let customData: Operation[] = data;
-
-		if (beginDate) {
-			customData = customData.filter((item) => {
-				const date = dayjs(item.date);
-				const begin = dayjs(beginDate);
-				return date >= begin;
-			});
-		}
-		if (endDate) {
-			customData = customData.filter((item) => {
-				const date = dayjs(item.date);
-				const end = dayjs(endDate);
-				return date <= end;
-			});
-		}
-		if (operatorName) {
-			customData = customData.filter(
-				(item) => item.operatorName === operatorName
-			);
-		}
-		setFilteredData(customData);
+	async function search() {
+		await fetch();
 	}
 }
 
 export default App;
-
-function getData(): OperationData[] {
-	return [
-		{
-			date: '01/01/2021',
-			value: 1000,
-			type: 'Transferencia',
-			operatorName: 'João',
-		},
-		{
-			date: '02/01/2021',
-			value: 200,
-			type: 'Transferencia',
-			operatorName: 'Maria',
-		},
-		{
-			date: '03/01/2021',
-			value: 150,
-			type: 'Transferencia',
-			operatorName: 'Pedro',
-		},
-		{
-			date: '29/01/2021',
-			value: 450,
-			type: 'Transferencia',
-			operatorName: 'Lucas',
-		},
-		{
-			date: '30/01/2021',
-			value: 300,
-			type: 'Transferencia',
-			operatorName: 'Ana',
-		},
-		{
-			date: '01/01/2021',
-			value: 100,
-			type: 'Transferencia',
-			operatorName: 'João',
-		},
-		{
-			date: '02/01/2021',
-			value: 200,
-			type: 'Transferencia',
-			operatorName: 'Maria 2',
-		},
-		{
-			date: '03/01/2021',
-			value: 150,
-			type: 'Transferencia',
-			operatorName: 'Pedro 2',
-		},
-		{
-			date: '29/01/2021',
-			value: 450,
-			type: 'Transferencia',
-			operatorName: 'Lucas 2',
-		},
-		{
-			date: '30/01/2021',
-			value: 300,
-			type: 'Transferencia',
-			operatorName: 'Ana',
-		},
-		{
-			date: '01/01/2021',
-			value: 100,
-			type: 'Transferencia',
-			operatorName: 'João',
-		},
-		{
-			date: '02/01/2021',
-			value: 200,
-			type: 'Transferencia',
-			operatorName: 'Maria',
-		},
-		{
-			date: '03/01/2021',
-			value: 150,
-			type: 'Transferencia',
-			operatorName: 'Pedro',
-		},
-		{
-			date: '29/01/2021',
-			value: 450,
-			type: 'Transferencia',
-			operatorName: 'Lucas',
-		},
-		{
-			date: '30/01/2021',
-			value: 300,
-			type: 'Transferencia',
-			operatorName: 'Ana',
-		},
-		{
-			date: '01/01/2021',
-			value: 100,
-			type: 'Transferencia',
-			operatorName: 'João',
-		},
-		{
-			date: '02/01/2021',
-			value: 200,
-			type: 'Transferencia',
-			operatorName: 'Maria',
-		},
-		{
-			date: '03/01/2021',
-			value: 150,
-			type: 'Transferencia',
-			operatorName: 'Pedro',
-		},
-		{
-			date: '29/01/2021',
-			value: 450,
-			type: 'Transferencia',
-			operatorName: 'Lucas',
-		},
-		{
-			date: '30/01/2021',
-			value: 300,
-			type: 'Transferencia',
-			operatorName: 'Ana',
-		},
-		{
-			date: '30/01/2021',
-			value: 300,
-			type: 'Transferencia',
-			operatorName: 'Ana',
-		},
-	];
-}
